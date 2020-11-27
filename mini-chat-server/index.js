c04d9539dd1ca3178bd1db00438735e0923e8bae
Daniil <imExpelliarmus@yandex.ru>
2020-11-27 10:17:57 +0800

    File replace

--- mini-chat-server/index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const { initDb } = require('./db');
-initDb();
-const server = require('./server');
-
-const PORT = process.env.PORT || 3001;
-
-server.listen(PORT, '0.0.0.0', () => console.log(`Server is live at http://localhost:${PORT}`));
